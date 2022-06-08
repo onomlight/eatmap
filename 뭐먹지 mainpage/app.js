f72a6c6afee1b0f3b63171be81fb56cd7baf9c53
onomlight <pass041@nate.com>
2022-06-08 11:27:54 +0900

    수정

--- 뭐먹지 mainpage/app.js
+++ 뭐먹지 mainpage/app.js
@@ -1,6 +1,6 @@
 var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
 var options = { //지도를 생성할 때 필요한 기본 옵션
-	center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+	center: new kakao.maps.LatLng(35.86719, 128.5961), //지도의 중심좌표.
 	level: 3 //지도의 레벨(확대, 축소 정도)
 };
 
