c751b584614f6fdf455860a4b6ce541fb34bd5ab
onomlight <pass041@nate.com>
2022-06-18 18:36:58 +0900

    ㅂㅓㅌㅡㄴㄹㅇㅏㅇㅜㅅㄹㅏㅇㅣㅅㅅㅏㄱㅈㅔ

--- 뭐먹지 mainpage/app.js
+++ 뭐먹지 mainpage/app.js
@@ -14,7 +14,7 @@
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div  
     mapOption = { 
         center: new kakao.maps.LatLng(35.86719, 128.5961), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
+        level: 4 // 지도의 확대 레벨
     };
 
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
